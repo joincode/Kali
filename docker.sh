2172a835b927fbc052ad4457996ac8781620b8b6
Roberto Lima <r.beto@hotmail.com>
2017-12-12 23:42:22 -0200

    Install Docker
    Install docker on Kali Linux

---
+++ docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Install docker Kali Linux 2017.x 
+#=============script install docker====================
+apt-get update
+apt-get install -y apt-transport-https ca-certificates
+apt-get install dirmngr
+
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \ --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo 'deb https://apt.dockerproject.org/repo debian-stretch main' > \ /etc/apt/sources.list.d/docker.list
+
+apt-get update
+
+apt-get install docker-engine
+
+service docker start
+
+sleep 5
+
+#Test Docker
+docker run hello-world
